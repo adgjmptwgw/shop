bd37f2925cd312b4196673648781675e3dda0e60
adgjmptwgw <adgjmptwgw@gmail.com>
2021-03-12 09:37:49 +0900

    first commit

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,7 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'success'; // ヘルスチェック用
+    return view('welcome');
 });
 
-Route::get('/friend/image/{userId}', 'Web\ImageController@show')->name('web.image.get');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
